65307cb9e305781408d84f097ee010a20692e181
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-06 14:10:15 +0530

    Running python script

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const {
   PythonShell
 } = require('python-shell')
 
-PythonShell.runString('x=1+1;print(x)', null, function (err, result) {
-  if (err) throw err;
-  console.log(result);
-});
+// PythonShell.runString('x=1+1;print(x)', null, function (err, result) {
+//   if (err) throw err;
+//   console.log(result);
+// });
+
+PythonShell.run('main.py', null, (err, results) => {
+  if (err) {
+    throw err;
+  }
+  console.log('results', results[0])
+})
